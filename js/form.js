560fc0a189b381bff5a4451a2c8366c226a783b4
svetlivaksevski <svetlivaksevski@gmail.com>
2024-02-22 13:43:49 +0100

    Separate tags

--- js/form.js
+++ js/form.js
@@ -5,20 +5,20 @@ document.querySelector('[data-js="form"]').addEventListener('submit', function(e
 	
     const question = document.getElementById('yourQuestion').value;
     const answer = document.getElementById('yourAnswer').value;
-    const tags = document.getElementById('yourTags').value;
-	
+	const tags = document.getElementById('yourTags').value.split(' ');
+
+    const tagsHTML = tags.map(tag => `<div class="tags">#${tag}</div>`).join('');
+
     const cardHTML = `
 	<section class="card"><h2 class="card__heading">${question}</h2>
 	<img src="./img/bookmark.png" class="bmimg" data-js="bookmarkimg">
 	<button class="buttons" data-js="button">Show answer</button>
 	<p class="hidden_answer" data-js="answer">${answer}</p>
 	<div class="dots">
-	<span class="tags">#${tags}</span> 
+	${tagsHTML}
 	</div>
 	</section>
     `;
-	
-	
 	
     const cardsContainer = document.getElementById('cardsContainer');
     cardsContainer.insertAdjacentHTML('beforeend', cardHTML);
@@ -67,15 +67,28 @@ const form = document.querySelector('[data-js="form"]');
 const textareas = document.querySelectorAll('textarea');
 const infos = document.querySelectorAll('[data-js="info"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
+const charactersAreaOne = document.querySelector('[data-js="changetextOne"]')
+const charactersAreaTwo = document.querySelector('[data-js="changetextTwo"]')
 
 textareas.forEach((textarea, index) => {
   infos[index].textContent = max - textarea.value.length;
 
   textarea.addEventListener('input', function() {
-    infos[index].textContent = max - this.value.length;
+    infos[index].textContent = max - this.value.length; 
     textarea.style.backgroundColor = this.value.length >= max ? 'var(--pinkcolor)' : '';
-  });
 });
+});
+
+
+// charactersAreaOne.addEventListener('change', function() {
+
+// 	if (infos[index].textContent < 2) {
+// 		charactersAreaOne.textContent = "character left " + (max - this.value.length); 
+// 	} else {
+// 		charactersAreaOne.textContent = "characters left " + (max - this.value.length);
+// 	}		  	 
+	
+// });
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
