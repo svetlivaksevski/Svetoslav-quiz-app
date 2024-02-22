18e35682e72db928d1c854ca366de66961f8563f
svetlivaksevski <svetlivaksevski@gmail.com>
2024-02-22 15:59:05 +0100

    Made counter change when it's character and characters

--- js/form.js
+++ js/form.js
@@ -57,46 +57,34 @@ document.querySelector('[data-js="form"]').addEventListener('submit', function(e
 	document.getElementById('yourQuestion').value = '';
     document.getElementById('yourAnswer').value = '';
     document.getElementById('yourTags').value = '';
+	charactersAreaOne.textContent = 'characters left 150';
+	yourQuestion.style.backgroundColor = '';
+	charactersAreaTwo.textContent = 'characters left 150';
+	yourAnswer.style.backgroundColor = '';
 });
 
 
 // Count the characters 
 
 const max = 150;
-const form = document.querySelector('[data-js="form"]');
-const textareas = document.querySelectorAll('textarea');
-const infos = document.querySelectorAll('[data-js="info"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
 const charactersAreaOne = document.querySelector('[data-js="changetextOne"]')
 const charactersAreaTwo = document.querySelector('[data-js="changetextTwo"]')
-
-textareas.forEach((textarea, index) => {
-  infos[index].textContent = max - textarea.value.length;
-
-  textarea.addEventListener('input', function() {
-    infos[index].textContent = max - this.value.length; 
-    textarea.style.backgroundColor = this.value.length >= max ? 'var(--pinkcolor)' : '';
-});
-});
+const yourQuestion = document.querySelector('[data-js="yourquestion"]')
+const yourAnswer = document.querySelector('[data-js="youranswer"]')
 
 
-// charactersAreaOne.addEventListener('change', function() {
+yourQuestion.addEventListener('input', function() {
+	charactersAreaOne.textContent = `${((max - this.value.length) < 2 ? "character" : "characters")} left ${max - this.value.length}`
+	yourQuestion.style.backgroundColor = this.value.length >= max ? 'var(--pinkcolor)' : '';
 
-// 	if (infos[index].textContent < 2) {
-// 		charactersAreaOne.textContent = "character left " + (max - this.value.length); 
-// 	} else {
-// 		charactersAreaOne.textContent = "characters left " + (max - this.value.length);
-// 	}		  	 
-	
-// });
+});	 
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
+yourAnswer.addEventListener('input', function() {
+	charactersAreaTwo.textContent = `${((max - this.value.length) < 2 ? "character" : "characters")} left ${max - this.value.length}`
+	yourAnswer.style.backgroundColor = this.value.length >= max ? 'var(--pinkcolor)' : '';
   
-	textareas.forEach((textarea, index) => {
-	  textarea.value = '';
-	  infos[index].textContent = max;
-	  textarea.style.backgroundColor = '';
-	});
-  });
-  
+});	 
+
+
+
